5628516d88db24fa9c2fbe36fef480b2f42d6b77
Jochen Stärk <jstaerk@usegroup.de>
2026-02-17 10:37:32 +0100

    conversion runs again, albeit with haardcoded path

--- src/main/java/org/mustangproject/server/controller/MustangController.java
+++ src/main/java/org/mustangproject/server/controller/MustangController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import it.infocert.eigor.api.*;
+
 import java.io.*;
 import java.util.Locale;
 
@@ -30,85 +31,71 @@ import javax.xml.transform.TransformerException;
 @Validated
 public class MustangController {
 
-  private static final Logger LOGGER = LogManager.getLogger(MustangController.class);
-  @Value("${mustang.additionalLog}")
-  protected String additionalLog;
+    private static final Logger LOGGER = LogManager.getLogger(MustangController.class);
+    @Value("${mustang.additionalLog}")
+    protected String additionalLog;
 
-    protected EigorApi api;
+    @Operation(
+            summary = "Returns the transformation from UBL to CII.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Returns the transformation from UBL to CII.",
+                            useReturnTypeSchema = true),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+            })
+    @RequestMapping(value = "/eigor", method = RequestMethod.POST, consumes = MediaType.APPLICATION_XML_VALUE)
+    public @ResponseBody HttpEntity<String> eigor(@RequestParam("sourceFormat") @Parameter(example = "ubl") String sourceFormat, @RequestParam("destFormat") @Parameter(example = "cii") String destFormat, @RequestBody @Parameter String xml, @RequestHeader(value = "USERNAME", required = false) String username)
+            throws IOException, ConfigurationException, SecurityException {
+        LOGGER.info("Operation: eigor;User: " + username + ";" + additionalLog);
+        ConversionResult<byte[]> cr = null;
+
+        if ((!sourceFormat.toLowerCase().equals("cii") && !sourceFormat.toLowerCase().equals("ubl") && !sourceFormat.toLowerCase().equals("fatturapa"))) {
+            throw new IllegalArgumentException("source format must be cii, ubl or fatturapa");
+        }
+
+        if ((!destFormat.toLowerCase().equals("cii") && !destFormat.toLowerCase().equals("ubl") && !destFormat.toLowerCase().equals("fatturapa"))) {
+            throw new IllegalArgumentException("destination format must be cii, ubl or fatturapa");
+        }
 
 
-    @PostConstruct
-    public void initialize() {
         try {
-            api =
+            EigorApi api =
                     new EigorApiBuilder()
                             .enableForce()
                             .build(); // or new EigorApiBuilder().withCustomConfiguration().build();
 
-        } catch (ConfigurationException e) {
+            cr =
+                    api.convert(
+                            sourceFormat.toLowerCase(),
+                            destFormat.toLowerCase(),
+                            new ByteArrayInputStream(xml.getBytes()));
+            return new HttpEntity<String>(new String(cr.getResult()));
+        } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage(), e);
+            return new HttpEntity<String>(new String("Error"));
         }
+
     }
 
-  @Operation(
-      summary = "Returns the transformation from UBL to CII.",
-      responses = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Returns the transformation from UBL to CII.",
-            useReturnTypeSchema = true),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Bad Request",
-            content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
-      })
-  @RequestMapping(value = "/eigor", method = RequestMethod.POST, consumes = MediaType.APPLICATION_XML_VALUE)
-  public @ResponseBody HttpEntity<String> eigor(@RequestParam("sourceFormat") @Parameter(example = "ubl") String sourceFormat, @RequestParam("destFormat") @Parameter(example = "cii") String destFormat, @RequestBody @Parameter String xml, @RequestHeader(value = "USERNAME", required = false) String username)
-      throws IOException, ConfigurationException, SecurityException {
-    LOGGER.info("Operation: eigor;User: " + username + ";" + additionalLog);
-    ConversionResult<byte[]> cr = null;
 
-    try {
-
-      if ((!sourceFormat.toLowerCase().equals("cii")&&!sourceFormat.toLowerCase().equals("ubl")&&!sourceFormat.toLowerCase().equals("fatturapa"))) {
-        throw new IllegalArgumentException("source format must be cii, ubl or fatturapa");
-      }
-
-      if ((!destFormat.toLowerCase().equals("cii")&&!destFormat.toLowerCase().equals("ubl")&&!destFormat.toLowerCase().equals("fatturapa"))) {
-        throw new IllegalArgumentException("destination format must be cii, ubl or fatturapa");
-      }
-
-      cr =
-          api.convert(
-              sourceFormat.toLowerCase(),
-              destFormat.toLowerCase(),
-                  new ByteArrayInputStream( xml.getBytes() ));
-    } catch (Exception e) {
-      LOGGER.error(e.getMessage(), e);
+    @Operation(
+            summary = "Healthcheck. Just request a ping, will respond with a 'pong'",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Healthcheck. Just request a ping, will respond with a 'pong'", useReturnTypeSchema = true),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+            }
+    )
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public String ping(@RequestHeader(value = "USERNAME", required = false) String username) throws FileNotFoundException, UnsupportedEncodingException, TransformerException {
+        LOGGER.info("Operation: ping;User: " + username + ";" + additionalLog);
+        return "pong";
     }
-
-    return new HttpEntity<String>(new String(cr.getResult()));
-  }
-
-
-
-
-  @Operation(
-          summary = "Healthcheck. Just request a ping, will respond with a 'pong'",
-          responses = {
-                  @ApiResponse(responseCode = "200", description = "Healthcheck. Just request a ping, will respond with a 'pong'", useReturnTypeSchema = true),
-                  @ApiResponse(
-                          responseCode = "400",
-                          description = "Bad Request",
-                          content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
-          }
-  )
-  @RequestMapping(value = "/ping", method = RequestMethod.GET)
-  public String ping(@RequestHeader(value = "USERNAME", required = false) String username) throws FileNotFoundException, UnsupportedEncodingException, TransformerException {
-    LOGGER.info("Operation: ping;User: " + username + ";" + additionalLog);
-    return "pong";
-  }
 
 }
